2105b3136e4413db75f8d69ab9820e481564f1ec
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2025-01-25 22:35:30 +1030

    Add verbose

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ import folium
 
 geojson_file = "datasets/RoadCrashes_GDA2020.geojson"
 
+
+print(f"Collecting data from dataset: {geojson_file}")
 with open(geojson_file, 'r') as file:
     geojson_data = json.load(file)
 
-map_center = [-37.8301386, 140.7842627]
+map_center = [30.0002, 136.2092]
+print(f"Setting map (center) location to {map_center[0]}, {map_center[1]}")
+
+print("Generating map")
 m = folium.Map(location=map_center, zoom_start=10)
 
 
 folium.GeoJson(geojson_data).add_to(m)
+print("Saving generated HTML map")
+map_file = 'rcr_interactive_map.html'
+
+m.save(map_file)
+print(f"Map generated: {map_file}")
 
 m.save('rcr_interactive_map.html')
