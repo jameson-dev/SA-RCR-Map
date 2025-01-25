17d93abb5f136f4b42327e9ee44ac2e096668760
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2025-01-25 22:23:27 +1030

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+import folium
+
+geojson_file = "RoadCrashes_GDA2020.geojson"
+
+with open(geojson_file, 'r') as file:
+    geojson_data = json.load(file)
+
+map_center = [-37.8301386, 140.7842627]
+m = folium.Map(location=map_center, zoom_start=10)
+
+
+folium.GeoJson(geojson_data).add_to(m)
+
+m.save('rcr_interactive_map.html')
